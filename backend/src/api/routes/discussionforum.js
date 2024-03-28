f7b310b28b5b42e3928fbf4c1f42f4fbf1df2522
Sushank Saini <sushank.saini@dal.ca>
2024-03-28 15:46:51 -0300

    added routes for the api of addding a reply

--- backend/src/api/routes/discussionforum.js
+++ backend/src/api/routes/discussionforum.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import {saveDiscussionPost} from '../controllers/discussionforum/index.js'
+import {saveDiscussionPost, saveReply} from '../controllers/discussionforum/index.js'
 import { getAllPosts } from "../controllers/discussionforum/index.js";
 
 const router = Router();
 
 router.post("/add-post", saveDiscussionPost);
 router.get("/get-all-posts", getAllPosts);
+router.post("/add-reply", saveReply);
 export default router;
