4a36110248f8c5c3d029320a221e4fc8c2f5c42b
Sushank Saini <sushank.saini@dal.ca>
2024-03-27 22:32:25 -0300

    modified the discussionforum model to add timmestamp flag

--- backend/src/models/discussionforum/mainpost.js
+++ backend/src/models/discussionforum/mainpost.js
@@ -6,15 +6,17 @@ const replySchema= new Schema ({  //schema represents a document in a collection
     userName: String,
     description: String,
     date: String
-})
+});
 
 const mainPostSchema=new Schema ({   //schema represents a document in a collection
     userName: String,
     title: String,
     description : String,
     date: String,
-    replies:[replySchema]
-})
+    replies:[replySchema]},
+    {
+        timestamps: true //for createdAt and updatedAt fields
+    });
 
-const DiscussionPost= mongoose.model("DiscussionPost", mainPostSchema); //model will be a collection in thr db
+const DiscussionPost= mongoose.model("DiscussionPost", mainPostSchema); //model will be a collection in the dbs
 export default DiscussionPost;
