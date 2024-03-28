d9190e64275b9618be746896bdbaf32d752616c3
Sushank Saini <sushank.saini@dal.ca>
2024-03-28 20:14:00 -0300

    added buiness logic to delete a post

--- backend/src/api/controllers/discussionforum/index.js
+++ backend/src/api/controllers/discussionforum/index.js
@@ -1,3 +1,4 @@
 export {default as saveDiscussionPost } from './addpost.js'
 export {default as getAllPosts} from './getallposts.js'
 export {default as saveReply} from './addreply.js'
+export {default as deletePost} from './deletepost.js'
