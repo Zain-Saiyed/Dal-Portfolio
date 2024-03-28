daebde6cd1b5be3fd1bf364219b7741a1fa0590e
boon <bn540239@dal.ca>
2024-03-28 06:53:13 -0300

    created get project by id api in backend

--- backend/src/api/controllers/collaboration/fetch_projects.js
+++ backend/src/api/controllers/collaboration/fetch_projects.js
@@ -2,7 +2,9 @@ import { CollabProjects } from "../../../models/index.js";
 
 export default async (req, res) => {
   try {
-    const projects = await CollabProjects.find();
+    //fetch projects based on the user_id
+    const { user_id } = req.query;
+    const projects = await CollabProjects.find({ user_id });
     return res.status(200).json(projects);
   } catch (error) {
     return res.status(500).json({ error: "Failed to fetch projects" });
--- backend/src/api/routes/collaboration_route.js
+++ backend/src/api/routes/collaboration_route.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   fetchProjects,
   fetchResearch,
+  fetchProjectByid,
 } from "../controllers/collaboration/index.js";
 
 const router = Router();
 
 router.get("/fetch_projects", fetchProjects);
 router.get("/fetch_research", fetchResearch);
+router.get("/fetch_project", fetchProjectByid);
 
 export default router;
