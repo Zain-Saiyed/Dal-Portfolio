c3a38515a355306f5fdb2226c401edc84a94683d
Sushank Saini <sushank.saini@dal.ca>
2024-03-29 14:39:11 -0300

    added routes for the api of delete replt

--- backend/src/api/routes/discussionforum.js
+++ backend/src/api/routes/discussionforum.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {deletePost, saveDiscussionPost, saveReply} from '../controllers/discussionforum/index.js'
+import {deletePost, deleteReply, saveDiscussionPost, saveReply} from '../controllers/discussionforum/index.js'
 import { getAllPosts } from "../controllers/discussionforum/index.js";
 
 const router = Router();
@@ -8,4 +8,5 @@ router.post("/add-post", saveDiscussionPost);
 router.get("/get-all-posts", getAllPosts);
 router.post("/add-reply", saveReply);
 router.post("/delete-post", deletePost)
+router.post("/delete-reply", deleteReply)
 export default router;
