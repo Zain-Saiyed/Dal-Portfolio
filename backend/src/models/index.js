bdfabc3229131c0513f896d4c7ac233fe26a5d75
Mohammed Noor ul Hasan Kothaliya <mh478572@dal.ca>
2024-03-31 00:13:53 -0300

    rebase main

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,6 +3,7 @@ import tokenModel from "./token.js";
 import logModel from "./log.js";
 import portfolioModel from "./portfolio.js";
 import discussionPostModel from './discussionforum/mainpost.js'
+import verificationModel from './verification.js'
 
 export const User = userModel;
 export const Token = tokenModel;
@@ -10,4 +11,4 @@ export const Log = logModel;
 export const Portfolio = portfolioModel;
 
 export const DiscussionPost= discussionPostModel;
-export const Verification = verficationModel;
+export const Verification = verificationModel;
