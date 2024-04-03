a3c2d20d6a2c60be0adb8e11d76d335d02a0fbb9
boon <bn540239@dal.ca>
2024-04-03 03:15:41 -0300

    removed conflict comments

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -2,32 +2,19 @@ import { Router } from "express";
 import user from "./user.js";
 import collaboration from "./collaboration_route.js";
 import profile from "./profile.js";
-<<<<<<< HEAD
-import portfolio from "./portfolio.js";
-import search from "./portfolio_search.js";
-import discussionforum from "./discussionforum.js";
-=======
 import portfolio from './portfolio.js';
 import search  from "./portfolio_search.js";
 import discussionforum from './discussionforum.js';
 import authMiddleware from "../middlewares/auth.js";
->>>>>>> a7a2b56761f884e051ed912b9e4957f83538d8c1
 
 const router = Router();
 
 router.use("/user", user);
-<<<<<<< HEAD
 router.use("/collaboration", collaboration);
 router.use("/user", user);
 router.use("/profile", profile);
 router.use("/portfolio", portfolio);
 router.use("/search", search);
 router.use("/discussionforum", discussionforum);
-=======
-router.use("/profile", authMiddleware, profile);
-router.use('/portfolio', portfolio);
-router.use('/search',search);
-router.use('/discussionforum', discussionforum );
->>>>>>> a7a2b56761f884e051ed912b9e4957f83538d8c1
 
 export default router;
